1b9175e14d19065fe025a24b62e7761142c71ea5
Oluwaseun Akeju <134845274+Nikkaburger@users.noreply.github.com>
2023-07-03 22:30:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showConfirmation() {
+    // Show the confirmation text
+    document.getElementById("confirmationText").style.display = "block";
+    // Prevent form submission (remove this line if you want to submit the form)
+    return false;
+  }
